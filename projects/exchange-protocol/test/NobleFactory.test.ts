73d936ff652cac0bdae6baedea3b33977eaf5ad5
Tenzija <milosmiskov.miki@gmail.com>
2023-01-22 22:05:18 +0100

    NobleFactory test without createPair and reverts

--- projects/exchange-protocol/test/NobleFactory.test.ts
+++ projects/exchange-protocol/test/NobleFactory.test.ts
@@ -1,39 +1,38 @@
-// import chai, { expect } from 'chai'
-// import { Contract, constants } from 'ethers'
-// import bigNumberify from "bignumberify";
-// import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle'
+import chai, { expect } from 'chai'
+import { Contract, constants } from 'ethers'
+import bigNumberify from "bignumberify";
+import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle'
 
-// import { getCreate2Address } from './shared/utilities'
-// import { factoryFixture } from './shared/fixtures'
+import { getCreate2Address } from './shared/utilities'
+import { factoryFixture } from './shared/fixtures'
 
-// import NoblePair from '../artifacts/contracts/NoblePair.sol/NoblePair.json'
+import NoblePair from '../artifacts/contracts/NoblePair.sol/NoblePair.json'
 
-// const TEST_ADDRESSES: [string, string] = [
-//   '0x1000000000000000000000000000000000000000',
-//   '0x2000000000000000000000000000000000000000'
-// ]
+const TEST_ADDRESSES: [string, string] = [
+  '0x1000000000000000000000000000000000000000',
+  '0x2000000000000000000000000000000000000000'
+]
 
-// describe('NobleFactory', () => {
-//   const provider = new MockProvider({
-//     hardfork: 'istanbul',
-//     mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
-//     gasLimit: 9999999
-//   })
-//   const [wallet, other] = provider.getWallets()
-//   const loadFixture = createFixtureLoader(provider, [wallet, other])
+describe('NobleFactory', () => {
+  const provider = new MockProvider({
+    hardfork: 'istanbul',
+    mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
+    gasLimit: 9999999
+  })
+  const [wallet, other] = provider.getWallets()
+  const loadFixture = createFixtureLoader(provider, [wallet, other])
 
-//   let factory: Contract
-//   beforeEach(async () => {
-//     const fixture = await loadFixture(factoryFixture)
-//     console.log("fixture: ", fixture)
-//     factory = fixture.factory
-//   })
+  let factory: Contract
+  beforeEach(async () => {
+    const fixture = await loadFixture(factoryFixture)
+    factory = fixture.factory
+  })
 
-//   it('feeTo, feeToSetter, allPairsLength', async () => {
-//     expect(await factory.feeTo()).to.eq(constants.AddressZero)
-//     expect(await factory.feeToSetter()).to.eq(wallet.address)
-//     expect(await factory.allPairsLength()).to.eq(0)
-//   })
+  it('feeTo, feeToSetter, allPairsLength', async () => {
+    expect(await factory.feeTo()).to.eq(constants.AddressZero)
+    expect(await factory.feeToSetter()).to.eq(wallet.address)
+    expect(Number(await factory.allPairsLength())).to.eq(0)
+  })
 
   // async function createPair(tokens: [string, string]) {
   //   const bytecode = `0x${NoblePair.evm.bytecode.object}`
@@ -42,8 +41,8 @@
   //     .to.emit(factory, 'PairCreated')
   //     .withArgs(TEST_ADDRESSES[0], TEST_ADDRESSES[1], create2Address, bigNumberify(1))
 
-  //   await expect(factory.createPair(...tokens)).to.be.reverted // UniswapV2: PAIR_EXISTS
-  //   await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted // UniswapV2: PAIR_EXISTS
+  //   await expect(factory.createPair(...tokens)).to.be.reverted // Noble: PAIR_EXISTS
+  //   await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted // Noble: PAIR_EXISTS
   //   expect(await factory.getPair(...tokens)).to.eq(create2Address)
   //   expect(await factory.getPair(...tokens.slice().reverse())).to.eq(create2Address)
   //   expect(await factory.allPairs(0)).to.eq(create2Address)
@@ -69,16 +68,17 @@
   //   expect(receipt.gasUsed).to.eq(2512920)
   // })
 
-  // it('setFeeTo', async () => {
-  //   await expect(factory.connect(other).setFeeTo(other.address)).to.be.revertedWith('UniswapV2: FORBIDDEN')
-  //   await factory.setFeeTo(wallet.address)
-  //   expect(await factory.feeTo()).to.eq(wallet.address)
-  // })
+  it('setFeeTo', async () => {
+    //await expect(factory.connect(other).setFeeTo(other.address)).to.be.revertedWith('Noble: FORBIDDEN')
+    await factory.setFeeTo(wallet.address)
+    expect(await factory.feeTo()).to.eq((wallet.address))
+  })
 
-  // it('setFeeToSetter', async () => {
-  //   await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('UniswapV2: FORBIDDEN')
-  //   await factory.setFeeToSetter(other.address)
-  //   expect(await factory.feeToSetter()).to.eq(other.address)
-  //   await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('UniswapV2: FORBIDDEN')
-  // })
-//})
+  it('setFeeToSetter', async () => {
+    //await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('Noble: FORBIDDEN')
+    await factory.setFeeToSetter(other.address)
+    expect(await factory.feeToSetter()).to.eq(other.address)
+    //await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('Noble: FORBIDDEN')
+  })
+
+})
