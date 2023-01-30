fab087f1996c6706b1fab18cbd823d099e4ed378
Austin McCall <macausty008@gmail.com>
2023-01-30 04:59:38 -0500

    Refactor

--- contracts.ts
+++ contracts.ts
@@ -1,22 +1,22 @@
 export default {
   nobleToken: {
-    4242: "0xa35062141fa33bca92ce69fed37d0e8908868aae",
-    1777: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
+    4242: "",
+    1777: "",
   },
   masterChef: {
-    4242: "0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9",
-    1777: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
+    4242: "",
+    1777: "",
   },
   sousChef: {
-    4242: "0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a",
-    1777: "0x6ab8463a4185b80905e05a9ff80a2d6b714b9e95",
+    4242: "",
+    1777: "",
   },
   multiCall: {
-    4242: "0x67ADCB4dF3931b0C5Da724058ADC2174a9844412",
-    1777: "0x1ee38d535d541c55c9dae27b12edf090c608e6fb",
+    4242: "",
+    1777: "",
   },
   claimRefund: {
     4242: "",
-    1777: "0xE7e53A7e9E3Cf6b840f167eF69519175c497e149",
+    1777: "",
   },
 };
